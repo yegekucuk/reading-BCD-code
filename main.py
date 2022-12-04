bfd272ea8a35fa13d079a0ab6103a3f1549f541d
yegekucuk <99672103+yegekucuk@users.noreply.github.com>
2022-12-04 22:06:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# Written in PyCharm
+def encode():
+    with open("enc.file", "r") as e:
+        exec(e.read())
+
+
+def decode():
+    with open("encrypted.txt", "r") as f:
+        characters = f.read().strip().split()
+        asciicharacters = list(map(lambda x: int(x, 2), characters))
+        p = "".join(list(map(chr, asciicharacters)))
+        print(p)
+
+
+# encode()
+decode()
+input()
